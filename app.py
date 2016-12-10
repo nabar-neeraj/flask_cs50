39663197632da7337f3b10db9aece7ee02a155fd
Neeraj Nabar <neerajnabar@gmail.com>
2016-12-10 21:38:53 +0530

    Functionality added to edit posts

--- app.py
+++ app.py
@@ -36,6 +36,28 @@ def create_post():
 	# Redirect the user to the homepage
 	return redirect(url_for('home'))
 
+@app.route('/delete_post/<post_id>', methods=['GET'])
+def delete_post(post_id):
+	print "Selected post",Post.get(id=post_id)
+	post_to_delete = Post.get(id=post_id)
+	# print "Removing",post_to_delete['title']
+	post_to_delete.delete_instance()
+	return redirect(url_for('home'))
+
+@app.route('/edit_post/<post_id>', methods=['GET','POST'])
+def edit_post(post_id):
+	print "Selected post",Post.get(id=post_id)
+	post_to_update = Post.get(id=post_id)
+	if request.method == 'GET':
+		return render_template('edit.html',post_info = post_to_update)
+	if request.method == 'POST':
+		print request.form['title']
+		print request.form['text']
+		post_to_update.title = request.form['title']
+		post_to_update.text = request.form['text']
+		post_to_update.save()
+		return redirect(url_for('home'))
+	
 
 if __name__ == '__main__':
 	app.run(debug=True)
